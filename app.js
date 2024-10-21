0814cc274e15f7665abe2b2fe3a6168e4c5f4bfa
crgiocolman <sacsy.py@gmail.com>
2024-10-21 09:45:11 -0300

    Servidor local https

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const axios = require('axios');
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const os = require('os');
 
-//inicializacion
+// Inicialización
 const app = express();
-const domain = "http://192.168.1.10";
+const localIP = getLocalIPAddress();
+const domain = `https://${localIP}`;
+//const domain = "https://192.168.100.26";
 const port = process.env.PORT || 3000;
 
-//importacion de rutas
+// Cargar los archivos de la clave privada y el certificado
+const sslOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'server.key')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'server.cert'))
+};
+
+// Importación de rutas
 const DahuaRoute = require("./src/routes/DahuaRoute");
 
-//configuraciones
-app.use(bodyParser.json()); //Middleware para parsear JSON
+// Configuraciones
+app.use(bodyParser.json()); // Middleware para parsear JSON
 app.use(cors());
 
 // Middleware para permitir CORS
@@ -24,7 +36,7 @@ app.use((req, res, next) => {
 
 app.use(express.json()); // Parsear JSON
 
-//uso de rutas
+// Uso de rutas
 app.use("/api", DahuaRoute);
 app.get("/", (req, res) => {
   res.status(200).send({ mensaje: "Hello WTC" })
@@ -61,7 +73,7 @@ app.post('/dahua/login', async (req, res) => {
       const randomKey = firstLoginResponse.data.randomKey;
       const publicKey = firstLoginResponse.data.publicKey;
 
-      //Realiza el segundo login
+      // Realiza el segundo login
       const signature = generateSignature(userName, password, realm, randomKey);
 
       const secondLoginResponse = await axios.post(`${domain}:80/brms/api/v1.0/accounts/authorize`, {
@@ -99,7 +111,24 @@ function generateSignature(username, password, realm, randomKey) {
   return signature;
 }
 
-//inicializar server
-app.listen(port, () => {
-  console.log(`Servidor Corriendo en ${domain}:${port}`)
-})
+function getLocalIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (let interfaceName in interfaces) {
+    for (let i = 0; i < interfaces[interfaceName].length; i++) {
+      const iface = interfaces[interfaceName][i];
+      
+      // Filtrar solo las direcciones IPv4 y evitar direcciones internas (como localhost)
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  return '127.0.0.1'; // Retorna localhost por defecto si no encuentra una IP externa
+}
+
+// Inicializar servidor HTTPS
+const server = https.createServer(sslOptions, app);
+
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor HTTPS corriendo en ${domain}:${port}`);
+});
